eb2b31c4769da43c46adc76b945f0cf6d3fce8e9
Chadwick Koo <chadwickkoo@outlook.com>
2025-02-28 07:08:06 +0300

    modified index.js file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages  = [];
+
+    for (let i = 0; i < names.length; i++ ) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+      
+    }
+return messages; 
+   
+}
+writeCards(["Ashley", "Sharon", "Alex"], "birthday")
+
+function countDown (){
+    let count = 10
+    while (count > -1) {
+        console.log(count);
+        count--;
+    }
+}
+countDown();
